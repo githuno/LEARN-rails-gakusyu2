18be9088f0e2859fe95867292dba427c3fc7e5ed
githuno <clas.warehouse@gmail.com>
2024-03-12 11:55:48 +0000

    create post method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,22 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :desc)
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: 'Post was successfully created.'
+    else
+      @posts = Post.all.order(created_at: :desc)
+      render :index
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
