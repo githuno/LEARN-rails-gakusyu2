acadddd9eb391213aad4cc31e88cb6daf2bdbf98
githuno <clas.warehouse@gmail.com>
2024-03-17 10:09:56 +0000

    フォロータイムラインおよびマイタイムラインを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,27 @@ class PostsController < ApplicationController
     render :timeline # index.htmlをレンダリングしないように変更
   end
 
+  def followings
+    @posts = current_user.following_posts.order(created_at: :desc)
+    @post = Post.new
+    render :timeline
+  end
+
+  def user
+    @user = User.find(params[:id])
+    @posts = @user.posts.order(created_at: :desc)
+    @post = Post.new
+    render :timeline
+  end
+
+  helper_method :current_action
+  def current_action
+    action_name
+  end
+
   def new
     @post = Post.new
+    render :form
   end
 
   def create
@@ -20,6 +39,26 @@ class PostsController < ApplicationController
       puts "Post could not be saved. Errors: #{@post.errors.full_messages}" # 保存失敗とエラーメッセージのログ出力
       redirect_to posts_path, alert: 'Post could not be created.'
     end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+    render :form
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: 'Post was successfully destroyed.'
   end
 
   private
