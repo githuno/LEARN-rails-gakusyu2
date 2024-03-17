af8abc829cc4e212b5f43ecdd8b601b57ec75366
githuno <clas.warehouse@gmail.com>
2024-03-17 11:54:42 +0000

    初期設定の再修正と、userモデル（20文字、アルファベット、、）のテスト通過

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User do
+  # ユーザー名（アルファベットのみ、スペース禁止、20文字以内）
+  describe 'バリデーション' do
+    it 'usernameがあれば有効であること' do
+      user = build(:user, username: 'testuser')
+      expect(user).to be_valid
+    end
+
+    it 'usernameがなければ無効であること' do
+      user = build(:user, username: nil)
+      user.valid?
+      expect(user.errors[:username]).to include("can't be blank")
+    end
+
+    it 'usernameが20文字以上なら無効であること' do
+      user = build(:user, username: 'a' * 21)
+      user.valid?
+      expect(user.errors[:username]).to include('is too long (maximum is 20 characters)')
+    end
+
+    it 'usernameが20文字なら有効であること' do
+      user = build(:user, username: 'a' * 20)
+      expect(user).to be_valid
+    end
+
+    it 'usernameがアルファベット以外なら無効であること' do
+      user = build(:user, username: 'あ')
+      user.valid?
+      expect(user.errors[:username]).to include('only allows letters')
+    end
+
+    it 'usernameがスペースを含むなら無効であること' do
+      user = build(:user, username: 'a a')
+      user.valid?
+      expect(user.errors[:username]).to include('only allows letters')
+    end
+  end
 end
