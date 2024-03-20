1b22241ef9c60cc6e531c70747929088b3f475ae
githuno <clas.warehouse@gmail.com>
2024-03-20 08:45:09 +0000

    BootStrapトーストの導入（controller.js）

---
+++ app/javascript/controllers/toast_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+    new bootstrap.Toast(this.element, { delay: 2000 }).show();
+    console.log("Connected to ToastController");
+  }
+}
