dbd6be861ac185a134dc028ffb81af7213989a9b
githuno <clas.warehouse@gmail.com>
2024-03-20 15:03:43 +0000

    rails/ujsの読み込み順変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-//= require rails-ujs
 
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+//= require rails-ujs
