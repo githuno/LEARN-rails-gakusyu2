afa73796aa82ecd21329e7456a8e3eac23a5cc99
githuno <clas.warehouse@gmail.com>
2024-03-28 12:27:13 +0000

    日時表示の変更（ビルドファイルの削除）

--- public/assets/controllers/toast_controller-385595478d887bc1a1ad0d39577cbd8fd130ad7cbecc62991805134060f7506d.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  connect() {
-    new bootstrap.Toast(this.element, { delay: 2000 }).show();
-    console.log("Connected to ToastController");
-  }
-};
